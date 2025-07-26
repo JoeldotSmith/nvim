8e8721d1e9f4de5583f6b4435bf4bd90c9555b27
Joel Smith <joelsmith2004@gmail.com>
2025-07-26 21:13:33 +0800

    feat(changed dashboard, added control-d/u to add zz afterwards)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { silent = true })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { silent = true })
+
+local map = vim.keymap.set
+
+vim.keymap.del("n", "<leader><tab>[")
+vim.keymap.del("n", "<leader><tab>]")
+-- New mappings
+map("n", "<C-d>", "<C-d>zz", { silent = true })
+map("n", "<C-u>", "<C-u>zz", { silent = true })
+map("n", "<leader><tab>h", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+map("n", "<leader><tab>l", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+map("n", "<leader><tab>g", "<cmd>tablast<cr>", { desc = "Last Tab" })
