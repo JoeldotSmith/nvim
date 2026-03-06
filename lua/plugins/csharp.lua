eca382edf202584b7f4eb7fed47f759de41c4d68
joeldotsmith2004 <joel.smith@enco.net.au>
2026-03-06 16:11:04 +0800

    feat(moved to roslyn)

--- lua/plugins/csharp.lua
+++ lua/plugins/csharp.lua
@@ -1,19 +1,81 @@
 return {
-  "nicholasmata/nvim-dap-cs",
-  dependencies = { "mfussenegger/nvim-dap" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "c_sharp",
+        "razor",
+      },
+    },
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      registries = {
+        "github:mason-org/mason-registry",
+        "github:Crashdummyy/mason-registry",
+      },
+      ensure_installed = {
+        "csharpier",
+        "roslyn",
+        "netcoredbg",
+      },
+    },
+  },
+  {
+    "seblyng/roslyn.nvim",
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    ft = { "cs", "razor" },
+    opts = {
+      -- your configuration comes here; leave empty for default settings
+    },
+  },
+  {
+    -- Debug Framework
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function()
+      require("config.nvim-dap")
+    end,
+    event = "VeryLazy",
+  },
+  { "nvim-neotest/nvim-nio" },
+  {
+    -- UI for debugging
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+    config = function()
+      require("config.nvim-dap-ui")
+    end,
+  },
+  {
+    "nvim-neotest/neotest",
+    requires = {
+      {
+        "Issafalcon/neotest-dotnet",
+      },
+    },
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
+  {
+    "Issafalcon/neotest-dotnet",
+    lazy = false,
+    dependencies = {
+      "nvim-neotest/neotest",
+    },
+  },
+  {
+    "ramboe/ramboe-dotnet-utils",
+    dependencies = { "mfussenegger/nvim-dap" },
+  },
 }
-
--- return {
---   "iabdelkareem/csharp.nvim",
---   dependencies = {
---     { "mason-org/mason.nvim", version = "1.3.0" },
---     { "mason-org/mason-lspconfig.nvim", version = "v0.13.0" },
---     "mfussenegger/nvim-dap",
---     "Tastyep/structlog.nvim",
---   },
---   lazy = true,
---   ft = "cs", -- load only for C# files
---   config = function()
---     require("csharp").setup()
---   end,
--- }
